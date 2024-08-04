355a8ae08f2f0f97ce2e430d9b65fd9df6178d14
hiba-tr <trabelsihiba88@gmail.com>
2024-08-04 17:04:53 +0100

    dotenv

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const http = require('http');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
